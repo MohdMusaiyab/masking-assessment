d250eb6a349f7bd404153d2db1b71fa9b1f80196
Mohd Musaiyab <mohdmusaiyab123@gmail.com>
2025-07-18 18:57:48 +0530

    Masking Improved

--- server/app/pii.py
+++ server/app/pii.py
@@ -1,12 +1,23 @@
 import re
 
 PII_PATTERNS = {
-    "aadhaar": r"\b\d{4}\s\d{4}\s\d{4}\b",
-    "phone": r"\b[6-9]\d{9}\b",
-    "dob": r"\b\d{2}[-/]\d{2}[-/]\d{4}\b",
-    "email": r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b",
-    "name": r"\b[A-Z][a-z]+\s[A-Z][a-z]+\b",  
-    "address": r"(?i)(address|street|road|nagar|vihar|sector)[^\n]*"
+    
+    "aadhaar": r"\b\d{4}[\s\-]?\d{4}[\s\-]?\d{4}\b",
+
+    
+    "phone": r"\b(?:\+91[-\s]?|0)?[6-9]\d{9}\b",
+
+    
+    "dob": r"\b\d{2}[-/.]\d{2}[-/.]\d{4}\b",
+
+    
+    "email": r"\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b",
+
+    
+    "name": r"\b([A-Z][a-z]{1,15})(\s[A-Z][a-z]{1,15}){1,2}\b",
+
+    
+    "address": r"\b(?:address|street|st\.|road|rd\.|block|nagar|sector|colony|lane|house|flat|vihar|phase|plot|building)\b[^\n,.;]*"
 }
 
 def is_pii(text: str) -> bool:
